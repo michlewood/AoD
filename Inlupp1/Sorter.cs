ae9951a9fbd536669492f9e12a7ed508a85eec7f
Michlewood <Michlewood@MW-LAPTOP>
2018-02-18 14:32:22 +0100

    Added merge sort, needs work.

--- Inlupp1/Sorter.cs
+++ Inlupp1/Sorter.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Inlupp1
 {
     internal class Sorter
     {
-        int[] numbers = { 5, -3, 22, 9001, -33, -105, -88, 1004, 5832, -2602 };
+        List<int> numbers = new List<int>() { 5, -3, 22, 9001, -33, -105, -88, 1004, 5832, -2602 };
         public Sorter()
         {
             Start();
@@ -12,28 +13,100 @@ namespace Inlupp1
 
         private void Start()
         {
-            PrintArray("Unsorted", numbers);
+            PrintList("Unsorted", numbers);
 
-            PrintArray("BubbleSort", BubbleSort());
+            PrintList("BubbleSort", BubbleSort());
 
-            //MergeSort
+            PrintList("Unsorted", numbers);
+
+            PrintList("MergeSort", MergeSort());
+
+
 
             //QuickSort();
         }
 
-        private int[] BubbleSort()
+        private List<int> MergeSort()
         {
-            int[] ListToSort = numbers;
-            for (int i = ListToSort.Length - 2; i >= 0; i--)
+            List<int> list2A = numbers.GetRange(0, 2);
+            List<int> list2B = numbers.GetRange(2, 2);
+            List<int> list2C = numbers.GetRange(4, 2);
+            List<int> list2D = numbers.GetRange(6, 2);
+            List<int> list2E = numbers.GetRange(8, 2);
+
+            PrintList("unsorted", list2A);
+            list2A = SecondSort(list2A.GetRange(0,1), list2A.GetRange(1, 1));
+            list2B = SecondSort(list2B.GetRange(0,1), list2B.GetRange(1, 1));
+            list2C = SecondSort(list2C.GetRange(0,1), list2C.GetRange(1, 1));
+            list2D = SecondSort(list2D.GetRange(0,1), list2D.GetRange(1, 1));
+            list2E = SecondSort(list2E.GetRange(0,1), list2E.GetRange(1, 1));
+            PrintList("sorted", list2A);
+
+            List<int> list4A = SecondSort(list2A, list2B);
+            List<int> list4B = SecondSort(list2C, list2D);
+
+            List<int> List8 = SecondSort(list4A, list4B);
+
+            List<int> listToSort = SecondSort(List8, list2E);
+
+            return listToSort;
+        }
+
+        private List<int> SecondSort(List<int> list1, List<int> list2)
+        {
+            List<int> listCombined = new List<int>();
+
+            int list1ToCompare = 0;
+            int list2ToCompare = 0;
+            bool continueLoop = true;
+            while (continueLoop)
+            {
+                bool ListTwoFirst = CompareElements(list1[list1ToCompare], list2[list2ToCompare]);
+
+                if (ListTwoFirst)
+                {
+                    listCombined.Add(list2[list2ToCompare]);
+                    list2ToCompare++;
+                }
+                else
+                {
+                    listCombined.Add(list1[list1ToCompare]);
+                    list1ToCompare++;
+                }
+                if (list2ToCompare == list2.Count)
+                {
+                    listCombined.AddRange(list1.GetRange(list1ToCompare, list1.Count-list1ToCompare));
+                    continueLoop = false;
+                }
+                else if (list1ToCompare == list1.Count)
+                {
+                    listCombined.AddRange(list2.GetRange(list2ToCompare, list2.Count - list2ToCompare));
+                    continueLoop = false;
+                }
+            }
+            return listCombined;
+        }
+
+        private bool CompareElements(int element1, int element2)
+        {
+
+            if (element1 > element2) return true;
+            else return false;
+
+        }
+
+        private List<int> BubbleSort()
+        {
+            List<int> listToSort = numbers.GetRange(0,numbers.Count);
+            for (int i = listToSort.Count - 2; i >= 0; i--)
             {
                 int current = i;
                 int compare = current + 1;
-                while (ListToSort[current] > ListToSort[compare])
+                while (listToSort[current] > listToSort[compare])
                 {
-                    int temp = ListToSort[current];
-                    ListToSort[current] = ListToSort[compare];
-                    ListToSort[compare] = temp;
-                    if (current < ListToSort.Length - 2)
+                    listToSort = SwitchElements(listToSort, current, compare);
+
+                    if (current < listToSort.Count - 2)
                     {
                         current = compare;
                         compare++;
@@ -41,10 +114,19 @@ namespace Inlupp1
                 }
 
             }
-            return ListToSort;
+            return listToSort;
         }
 
-        private void PrintArray(string nameOfArray, int[] array)
+        private List<int> SwitchElements(List<int> listToSort, int current, int compare)
+        {
+            int temp = listToSort[current];
+            listToSort[current] = listToSort[compare];
+            listToSort[compare] = temp;
+
+            return listToSort;
+        }
+
+        private void PrintList(string nameOfArray, List<int> array)
         {
             Console.WriteLine($"{nameOfArray}:");
             foreach (var number in array)
